d6e2af5ca65356d6df818c058619d6b6c01e6637
Matt Thompson <matthewthompson118@gmail.com>
2023-12-18 14:42:40 -0400

    added more to userInput.js

--- lib:userInput.js
+++ lib:userInput.js
@@ -13,6 +13,19 @@ async function getUserInput() {
             message: 'Select a color for your logo',
             choices: ['red', 'green', 'blue,', 'yellow', 'purple'],
         },
-        
-    ]
+        {
+            type: 'list',
+            name:: 'shape',
+            message: 'Select a shape for your logo',
+            choices: ['circle', 'square', 'triangle'],
+        },
+    ];
+
+    return inquirer.prompt(questions);
 }
+
+function createLogo(text, color, shape) {
+
+}
+
+module.exports = {getuserInput, createLogo};
